727e3202c6d10ad435a86810b377aef93e9e0062
Ranger <mnipk1243@gmail.com>
2026-03-14 20:55:44 +0500

    added knowledge base folder, with agent class, ingestion class and retrieval class. Agent.py will contain the agent class, which will be the parent class of ingestion and retrieval classes

---
+++ KnBaseAgent/ingestion.py
@@ -0,0 +1,9 @@
+#ingestion class
+from langchain_community.document_loaders import PyPDFLoader
+
+class ingestion:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def ingest_document(self):
+        return 
