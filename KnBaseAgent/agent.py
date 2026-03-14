54b8f8174d024e23831a41bdfb1a5322cbce1b8f
Ranger <mnipk1243@gmail.com>
2026-03-15 02:51:17 +0500

    creating embeddings

--- KnBaseAgent/agent.py
+++ KnBaseAgent/agent.py
@@ -2,5 +2,5 @@
 
 from ingestion import ingestion
 
-ingestion_obj = ingestion('../resume2-10.pdf')
+ingestion_obj = ingestion('resume2-10.pdf')
 ingestion_obj.ingest_document()
--- KnBaseAgent/ingestion.py
+++ KnBaseAgent/ingestion.py
@@ -1,16 +1,25 @@
 #ingestion class
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class ingestion:
     def __init__(self, filepath):
         self.filepath = filepath
         self.loader = PyPDFLoader(self.filepath)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap = 100)
+        self.embedding = OllamaEmbeddings(
+            model=os.environ.get("EMBEDDING_MODEL")
+        )
 
     def ingest_document(self):
         doc = self._load_document()
-        texts = self._create_chunks(doc) 
+        chunks = self._create_chunks(doc) 
+        str_chunks = self._convert_doc_chunks_to_str(chunks)
+        self._create_embeddings_from_chunks(str_chunks)
         return 
 
     def _load_document(self):
@@ -18,5 +27,18 @@ class ingestion:
         return pdf_docs
 
     def _create_chunks(self, document_text):
-        texts = self.text_splitter.split_documents(document_text)
-        return texts
+        chunks = self.text_splitter.split_documents(document_text)
+        return chunks
+
+    def _convert_doc_chunks_to_str(self, chunks):
+        str_chunks = []
+        for chunk in chunks:
+            string_chunk = str(chunk)
+            str_chunks.append(string_chunk)
+        return str_chunks
+
+    def _create_embeddings_from_chunks(self, text_chunks):
+        embeddings = self.embedding.embed_documents(text_chunks) 
+        return
+
+    # need to store in vector database
