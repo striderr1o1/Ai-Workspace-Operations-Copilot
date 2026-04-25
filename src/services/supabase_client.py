8ebfa43583c39bf587860d044394a26046436292
Ranger <mnipk1243@gmail.com>
2026-04-25 23:22:46 +0500

    added supabase fetch

---
+++ src/services/supabase_client.py
@@ -0,0 +1,26 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+load_dotenv()
+
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_apikey = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(supabase_url, supabase_apikey)
+
+
+def fetch_room_data():
+    response = (supabase.table("rooms")
+    .select("*")
+    .execute())
+    print(response)
+    return response
+
+def update_room_data():
+    return
+
+def delete_room_data():
+    return
+
+def create_room_data():
+    return
